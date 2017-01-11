de011254c5edcde04452634eecd1501a36f8b7c0
subvertnormality <andy@subvertnormality.com>
2017-01-11 12:36:52 +0000

    Adding logic

--- detect.py
+++ detect.py
@@ -2,8 +2,18 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 import copy
+import time
+import requests
 
-MIN_MATCH_COUNT = 6
+
+cert_file_path = "certs/client.crt"
+key_file_path = "certs/client.key"
+cert = (cert_file_path, key_file_path)
+
+url = 'https://secure.playperception.com/game/playround/'
+url_local = 'https://secure.localhost/game/playround/'
+
+MIN_MATCH_COUNT = 4
 
 q1 = cv2.imread('images/1.png',0)
 q2 = cv2.imread('images/2.png',0)
@@ -14,8 +24,16 @@ q3l = cv2.imread('images/3l.png',0)
 q1r = cv2.imread('images/1r.png',0)
 q2r = cv2.imread('images/2r.png',0)
 q3r = cv2.imread('images/3r.png',0)
-scene = cv2.imread('scene2r.png',0) # trainImage
 
+truth = np.array([[[True,True],[True,False],[False,False],[False,True]],[[True,True],[True,False],[False,False],[False,True]],[[True,True],[True,False],[False,False],[False,True]],[[True,True],[True,False],[False,False],[False,True]]])
+one_bounds = np.array([[635, 0], [635, 163], [890, 163], [890, 0]])
+two_bounds = np.array([[890, 0], [890, 163], [1190, 163], [1190, 0]])
+three_bounds = np.array([[1190, 0], [1190, 163], [1445, 163], [1445, 0]])
+four_bounds = np.array([[610, 700], [610, 1080], [920, 1080], [920, 700]])
+five_bounds = np.array([[920, 700], [920, 1080], [1250, 1080], [1250, 700]])
+six_bounds = np.array([[1250, 700], [1250, 1080], [1650, 1080], [1650, 700]])
+
+bounds = [one_bounds, two_bounds, three_bounds, four_bounds, five_bounds, six_bounds]
 
 def do_transform(ref, scene):
 
@@ -48,18 +66,20 @@ def do_transform(ref, scene):
                 good.append(m)
 
         if len(good)>MIN_MATCH_COUNT:
-            src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-            dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+            try:
+                src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+                dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
 
-            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
-            matchesMask = mask.ravel().tolist()
+                M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+                matchesMask = mask.ravel().tolist()
 
-            h,w = o.shape
-            pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
-            dst = cv2.perspectiveTransform(pts,M)
+                h,w = o.shape
+                pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+                dst = cv2.perspectiveTransform(pts,M)
 
-            sceneTransform = copy.deepcopy(scene)
-
+                sceneTransform = copy.deepcopy(scene)
+            except:
+                print('Error finding match')
         else:
             # print("Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT))
             matchesMask = None
@@ -67,54 +87,75 @@ def do_transform(ref, scene):
             continue
         else:
             return dst
+
     return np.array([])
 
-q1dst = np.array([])
-q1dst = do_transform(q1, scene)
-if (q1dst.size == 0):
-    q1dst = do_transform(q1l, scene)
-if (q1dst.size == 0):
-    q1dst = do_transform(q1r, scene)
-q2dst = np.array([])
-q2dst = do_transform(q2, scene)
-if (q2dst.size == 0):
-    q2dst = do_transform(q2l, scene)
-if (q2dst.size == 0):
-    q2dst = do_transform(q2r, scene)
-q3dst = np.array([])
-q3dst = do_transform(q3, scene)
-if (q3dst.size == 0):
-    q3dst = do_transform(q3l, scene)
-if (q3dst.size == 0):
-    q3dst = do_transform(q3r, scene)
 
 
-truth = np.array([[[True,True],[True,False],[False,False],[False,True]],[[True,True],[True,False],[False,False],[False,True]],[[True,True],[True,False],[False,False],[False,True]],[[True,True],[True,False],[False,False],[False,True]]])
-one_bounds = np.array([[459, 378], [459, 444], [604, 444], [604, 378]])
-two_bounds = np.array([[459, 378], [459, 444], [604, 444], [604, 378]])
-three_bounds = np.array([[459, 378], [459, 444], [604, 444], [604, 378]])
-four_bounds = np.array([[459, 378], [459, 444], [604, 444], [604, 378]])
-five_bounds = np.array([[459, 378], [459, 444], [604, 444], [604, 378]])
-six_bounds = np.array([[459, 378], [459, 444], [604, 444], [604, 378]])
+
+while True:
+    try:
+        cap = cv2.VideoCapture(0)
+        cap.set(3,1920)
+        cap.set(4,1080)
+        ret, frame = cap.read()
+        cv2.imwrite("frame.jpg", frame)
+
+        # Display the resulting frame
+
+        q1dst = np.array([])
+        q1dst = do_transform(q1, frame)
+        if (q1dst.size == 0):
+            q1dst = do_transform(q1l, frame)
+        if (q1dst.size == 0):
+            q1dst = do_transform(q1r, frame)
+        if (q1dst.size == 0):
+            print('skipping because of 1')
+            continue
+        q2dst = np.array([])
+        q2dst = do_transform(q2, frame)
+        if (q2dst.size == 0):
+            q2dst = do_transform(q2l, frame)
+        if (q2dst.size == 0):
+            q2dst = do_transform(q2r, frame)
+        if (q2dst.size == 0):
+            print('skipping because of 2')
+            continue
+        q3dst = np.array([])
+        q3dst = do_transform(q3, frame)
+        if (q3dst.size == 0):
+            q3dst = do_transform(q3l, frame)
+        if (q3dst.size == 0):
+            q3dst = do_transform(q3r, frame)
+        if (q3dst.size == 0):
+            print('skipping because of 3')
+            continue
+        dsts = [q1dst, q2dst, q3dst]
 
 
-dsts = [q1dst, q2dst, q3dst]
-bounds = [one_bounds, two_bounds, three_bounds, four_bounds, five_bounds, six_bounds]
+        result = []
 
-result = []
+        for bound in bounds:
+            found = False
+            block = 1
+            for dst in dsts:
+                try:
+                    if np.equal(np.greater_equal(dst, bound), truth).all():
+                        result.append(block)
+                        found = True
+                except:
+                    print('No valid bounds found') 
+                block = block + 1
+            if not found:
+                result.append(0)
 
-for bound in bounds:
-    found = False
-    block = 1
-    for dst in dsts:
-        if np.equal(np.greater_equal(dst, bound), truth).all():
-            result.append(block)
-            found = True
-        block = block + 1
-    if not found:
-        result.append(0)
-        
-print(result)
+        print(result)
 
-# print(np.equal(np.greater_equal(q3dst, one_bounds), truth).all())
-plt.imshow(cv2.polylines(scene,[np.int32(one_bounds)],True,255,3, cv2.LINE_AA), 'gray'),plt.show()
+        if (1 in result and 2 in result and 3 in result and len(result) == 6):
+            print('posting')
+            r = requests.post(url, json={'attempt': result}, cert=cert, verify=False)
+        else:
+            print('ígnoring')
+    except Exception as e:
+        print(e)
+    time.sleep(1)
